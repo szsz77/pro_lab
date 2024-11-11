99aaa1dff471e7ed21d327f8aaa18d97a0350b99
szsz77 <sz2477@outlook.com>
2024-11-11 13:37:14 -0500

    add attributes.py with openai_key removed

---
+++ attributes.py
@@ -0,0 +1,16 @@
+ERROR_MAXIMUM_TRY_OUT = 3
+INFO_COLLECT_MAXIMUM_TRY_OUT = 3
+PREFERENCE_SLOTS = ['product_group', 'product_type', 'graphical_appearance', 'color_group', 'perceived_colour', 'perceived_colour_master', 'index', 'section', 'garment_group']
+VECTOR_STORE_PATH ='./vector_index_small/'
+CLIP_STRING_LENGTH = 200
+openai_api_key = REPLACE_WITH_OPENAI_KEY
+
+    # {"product_group": 'N/A',
+    #                 "product_type": 'N/A',
+    #                 "graphical_appearance": 'N/A',
+    #                 "color_group": 'N/A',
+    #                 "perceived_colour": 'N/A',
+    #                 "perceived_colour_master": 'N/A',
+    #                 "index": 'N/A',
+    #                 "section": 'N/A',
+    #                 "garment_group": 'N/A', }
